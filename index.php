1725c073a801fce5dfb6e96e1ebeca765206e232
rafieakmalh <74704272+rafieakmalh@users.noreply.github.com>
2020-12-17 15:47:32 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -109,6 +109,32 @@
                 <br><br>
                 <input type="submit" name="Submit" value="Submit Comment" class="Submit">
             </form>
+            <?php
+
+            if ($_POST['Submit']) {
+                print "<h1>Your comment has been submitted!</h1>";
+
+                $Name = $_POST['Name'];
+                $Comment = $_POST['Comment'];
+
+                #Get old comments
+                $old = fopen("comments.txt", "r+t");
+                $old_comments = fread($old, 1024);
+
+                #Delete everything, write down new and old comments
+                $write = fopen("comments.txt", "w+");
+                $string = "<b>" . $Name . "</b><br>" . $Comment . "<br>\n" . $old_comments;
+                fwrite($write, $string);
+                fclose($write);
+                fclose($old);
+            }
+
+            #Read comments
+            $read = fopen("comments.txt", "r+t");
+            echo "<br><br>Comments<hr>" . fread($read, 1024);
+            fclose($read);
+
+            ?>
         </div>
 
         <div class="col-md-4">
@@ -254,32 +280,5 @@
     </footer>
 
 </body>
-
-<?php
-
-if ($_POST['Submit']) {
-    print "<h1>Your comment has been submitted!</h1>";
-
-    $Name = $_POST['Name'];
-    $Comment = $_POST['Comment'];
-
-    #Get old comments
-    $old = fopen("comments.txt", "r+t");
-    $old_comments = fread($old, 1024);
-
-    #Delete everything, write down new and old comments
-    $write = fopen("comments.txt", "w+");
-    $string = "<b>" . $Name . "</b><br>" . $Comment . "<br>\n" . $old_comments;
-    fwrite($write, $string);
-    fclose($write);
-    fclose($old);
-}
-
-#Read comments
-$read = fopen("comments.txt", "r+t");
-echo "<br><br>Comments<hr>" . fread($read, 1024);
-fclose($read);
-
-?>
 
 </html>
